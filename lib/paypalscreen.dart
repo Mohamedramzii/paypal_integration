3e6cd330a62d22cfedd54fa2f53d15323f2b6b84
Muhammed Ramzy <57300921+Mohamedramzii@users.noreply.github.com>
2023-06-21 23:34:08 +0300

    Paypal Integration

---
+++ lib/paypalscreen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_paypal_checkout/flutter_paypal_checkout.dart';
+class CheckoutPage extends StatefulWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  State<CheckoutPage> createState() => _CheckoutPageState();
+}
+
+class _CheckoutPageState extends State<CheckoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "PayPal Checkout",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => PaypalCheckout(
+                sandboxMode: true,
+                clientId: "AUJAXEe3b8bwySah2HpIlzayW1QRyzkcfscKZaS_cMD6os0HcFb9f_G0iQF-AMDr_5fIVxHz2WI4qtjZ",
+                secretKey: "EFqWyjjpYcFZOpTr7PWZh4mKtqIykL4Voputj_PaLaY4h_mn2CPcAbLlgmaRvyJjUJj3y69j2MnC__en",
+                returnURL: "success.snippetcoder.com",
+                cancelURL: "cancel.snippetcoder.com",
+                transactions: const [
+                  {
+                    "amount": {
+                      "total": '70',
+                      "currency": "USD",
+                      "details": {
+                        "subtotal": '70',
+                        "shipping": '0',
+                        "shipping_discount": 0
+                      }
+                    },
+                    "description": "The payment transaction description.",
+                  
+                    "item_list": {
+                      "items": [
+                        {
+                          "name": "Apple",
+                          "quantity": 4,
+                          "price": '5',
+                          "currency": "USD"
+                        },
+                        {
+                          "name": "Pineapple",
+                          "quantity": 5,
+                          "price": '10',
+                          "currency": "USD"
+                        }
+                      ],
+                    }
+                  }
+                ],
+                note: "Contact us for any questions on your order.",
+                onSuccess: (Map params) async {
+                  print("onSuccess: $params");
+                },
+                onError: (error) {
+                  print("onError: $error");
+                  Navigator.pop(context);
+                },
+                onCancel: () {
+                  print('cancelled:');
+                },
+              ),
+            ));
+          },
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.white,
+            shape: const BeveledRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(1),
+              ),
+            ),
+          ),
+          child: const Text('Pay With Paypal'),
+        ),
+      ),
+    );
+  }
+}
